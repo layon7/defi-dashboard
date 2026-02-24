16287447f6c8c79f7b63f8adfc51ec0227eeb741
layon7 <58602416+layon7@users.noreply.github.com>
2026-02-24 21:27:07 +0000

    fix base path para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia 'defi-dashboard' por el nombre EXACTO de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/defi-dashboard/',
+  base: '/',
 })
